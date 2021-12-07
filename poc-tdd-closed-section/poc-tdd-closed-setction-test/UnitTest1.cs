78437f8f98fe752c3dfbf84e9f4f893c393b0830
bory-kb <s.kubo0527@gmail.com>
2021-12-07 14:11:23 +0900

    48グリーン

--- poc-tdd-closed-section/poc-tdd-closed-setction-test/UnitTest1.cs
+++ poc-tdd-closed-section/poc-tdd-closed-setction-test/UnitTest1.cs
@@ -14,8 +14,15 @@ namespace poc_tdd_closed_setction_test
         [Test]
         public void 下端点が3上端点が7のとき3カンマ7として返す()
         {
-            var ret = ClosedSection.ReturnClosedSection(3,7);
-            Assert.AreEqual("[3,7]",ret);
+            var ret = ClosedSection.ReturnClosedSection(3, 7);
+            Assert.AreEqual("[3,7]", ret);
+        }
+
+        [Test]
+        public void 下端点が4上端点が8のとき4カンマ8として返す()
+        {
+            var ret = ClosedSection.ReturnClosedSection(4, 8);
+            Assert.AreEqual("[4,8]", ret);
         }
     }
 }
